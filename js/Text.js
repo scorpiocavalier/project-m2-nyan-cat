96ddf8c7349d5bde0360117d7fee8e25f94da837
Arturo Chen <art.can.fly@gmail.com>
2020-06-30 11:13:41 -0400

    all comments removed for clarity

--- js/Text.js
+++ js/Text.js
@@ -1,10 +1,5 @@
-// This class is not used in the project yet.
 class Text {
-  // The constructor has three parameters. Here is an example of how you would create
-  // an instance of this class
   constructor(root, xPos, yPos) {
-    // We create a DOM element, set its CSS attributes then append it to the parent DOM element. We also
-    // set the \`domElement\` property of the instance to the newly created DOM element so we can update it later
     const div = document.createElement('div');
 
     div.style.position = 'absolute';
@@ -19,7 +14,6 @@ class Text {
     this.domElement = div;
   }
 
-  // This method is used to update the text displayed in the DOM element
   update(txt) {
     this.domElement.innerText = txt;
   }
